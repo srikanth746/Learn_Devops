4394b84b742dd5566deee4bf0bbac3c0126dec0e
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-03 13:02:06 +0530

    First shell script with Linux basics V2.2

--- shell-script/Linux_basics.sh
+++ shell-script/Linux_basics.sh
@@ -1,11 +1,14 @@
 ls
 pwd
 ls -la
-touch file2.txt
 vi file2.txt
 cat file2.txt
 sleep 10&
 lsblk
 uname -i
 ps -u
-yum remove python3 -y
+yum install python3 -y
+echo "Creating a python script"
+touch pythonexample.oy
+vi pythonexample.py
+python3 pythonexample.py

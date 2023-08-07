0a80cc8422b0d2750ff3094fd6f837a8812954fc
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-07 22:10:43 +0530

    Adding the shell script for Catalogue application of roboshop

---
+++ roboshop-shell/Catalogue.sh
@@ -0,0 +1,28 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+#Copying the service data to the catalogue location
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+#Adding the mongo db repo file
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+#Creating a directory to add the catalogue content
+
+mkdir /app
+cd /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+unzip /tmp/catalogue.zip
+npm install
+
+
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
---
+++ roboshop-shell/Redis.sh
@@ -0,0 +1,11 @@
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+yum module enable redis:remi-6.2 -y
+
+yum install redis -y
+
+vim /etc/redis.conf
+vim /etc/redis/redis.conf
+
+systemctl enable redis
+systemctl start redis

23e39e423cc89fdae3b9e4b9d45e0534553634b5
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-08 22:09:22 +0530

    Updated Cart script

--- roboshop-shell/Cart.sh
+++ roboshop-shell/Cart.sh
@@ -8,9 +8,10 @@ useradd roboshop
 
 mkdir /app
 
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl  -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
+cd /app
 npm install
 
 systemctl daemon-reload

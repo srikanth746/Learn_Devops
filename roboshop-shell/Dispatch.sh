98f2a731c186120aa8861a24c1fda37d9190c228
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-08 12:16:40 +0530

    Adding Dispatch shell script for Roboshop

---
+++ roboshop-shell/Dispatch.sh
@@ -0,0 +1,21 @@
+cp Dispatch.service /etc/systemd/system/dispatch.service
+
+yum install golang -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+
+cd /app
+unzip /tmp/dispatch.zip
+
+go mod init dispatch
+go get
+go build
+
+systemctl daemon-reload
+
+systemtctl enable dispatch
+systemctl restart dispatch

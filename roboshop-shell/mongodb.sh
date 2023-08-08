3f8c24cce95fb2907eec415716951400a07b92de
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-08 16:51:03 +0530

    Updated Listen Address of Mongo-DB.

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -3,7 +3,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
 #listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-vim /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 #Updating IP address got completed
 systemctl enable mongod
 systemctl restart mongod

85cb65b7c0da163a94b2ca52c330b9d04c7f2e75
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-09 20:33:28 +0530

    changed file name of rabbitMQ

--- roboshop-shell/rabbitMQ.sh
+++ roboshop-shell/rabbitMQ.sh
@@ -2,6 +2,6 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 yum install rabbitmq-server -y
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"

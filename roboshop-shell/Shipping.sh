9629552cc94abb4c7605bfae482f0696399b302a
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-09 20:02:45 +0530

    updated shipping shell script

--- roboshop-shell/Shipping.sh
+++ roboshop-shell/Shipping.sh
@@ -11,8 +11,8 @@ mv target/shipping-1.0.jar shipping.jar
 
 yum install mysql -y
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql.srilearndevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
+systemctl restart shipping

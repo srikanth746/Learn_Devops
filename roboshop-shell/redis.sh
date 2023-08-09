103d2d3763605b0ae5d4a8b63f907a048b949147
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-09 19:02:41 +0530

    redis updated shell script

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -1,11 +1,6 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-
 yum module enable redis:remi-6.2 -y
-
 yum install redis -y
 
-#Update the listen address to 0.0.0.0
-sed -i 's/127.0.0.1 to 0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
-
 systemctl enable redis
-systemctl restart redis
+systemctl start redis

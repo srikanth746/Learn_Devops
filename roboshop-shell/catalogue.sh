ee160c176fb234a7ef2754652e9d38e25fc75108
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-10 16:22:41 +0530

    Updated catalogue files and script

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -8,18 +8,13 @@ yum install nodejs -y
 
 useradd roboshop
 
-#Copying the service data to the catalogue location
-#cp catalogue.service /etc/systemd/system/catalogue.service
-
-#Adding the mongo db repo file
-#cp mongo.repo /etc/yum.repos.d/mongo.repo
-
 yum install mongodb-org-shell -y
 
 #Creating a directory to add the catalogue content
 
 mkdir /app
 cd /app
+pwd
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 unzip /tmp/catalogue.zip
 npm install

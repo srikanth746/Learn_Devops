9257273101290da76331ea2f8c98a09e851014cf
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-12 21:29:18 +0530

    Working on installing web server by debugging

---
+++ shell-script/print.sh
@@ -0,0 +1,21 @@
+echo "Practicing the print cmd and installing nginx"
+
+echo -e"\e[33Checking the nginx status\e[0m"
+
+yum list installed | grep nginx
+
+echo -e "\e[32mInstalling Nginx\e[0m"
+
+yum install nginx -y
+
+echo -e "\e[35mInstallation of nginx is completed\e[0m"
+
+echo -e "\e[34starting the nginx web service\e[0m"
+
+systemctl start nginx
+
+echo -e "\e[35checking the status of nginx\e[0m"
+systemctl status nginx
+
+echo -e "\e[36removing nginx\e[0m"
+yum remove nginx -y

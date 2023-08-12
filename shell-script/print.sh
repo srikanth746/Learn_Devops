5f0d8071a03888fe31f0099fd786898b55c71f0b
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-12 21:54:59 +0530

    Used echo cmd to install nginx and python.

--- shell-script/print.sh
+++ shell-script/print.sh
@@ -1,21 +1,27 @@
-echo "Practicing the print cmd and installing nginx"
-
-echo -e "\e[33m Checking the nginx status\e[0m"
+echo -e "\e[33mChecking the nginx server available\e[0m"
 
 yum list installed | grep nginx
 
-echo -e "\e[32m Installing Nginx\e[0m"
+echo -e "\e[32mInstalling Nginx\e[0m"
 
 yum install nginx -y
 
-echo -e "\e[35m Installation of nginx is completed\e[0m"
+echo -e "\e[35mInstallation of nginx is completed\e[0m"
 
-echo -e "\e[34 mstarting the nginx web service\e[0m"
+echo -e "\e[34mStarting the nginx web service\e[0m"
 
 systemctl start nginx
 
-echo -e "\e[35 mchecking the status of nginx\e[0m"
+echo -e "\e[35mChecking the status of nginx\e[0m"
 systemctl status nginx
 
-echo -e "\e[36m removing nginx\e[0m"
-yum remove nginx -y
+echo -e "\e[31mInstallation of python starts\e[0m"
+yum install python3 -y
+
+echo -e "\e[32mCreating and editing python file for execution\e[0m"
+touch sample1.py
+vim sample1.py
+echo -e "\e[34mPrinting the start pattern in python\e[0m"
+python3 sample1.py
+
+echo -e "\e[34mSuccessfully executed python script\e[0m"

848569376e72fdd958bd864367298b24a134b186
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-13 22:24:01 +0530

    Updated user shell script.

--- roboshop-shell/user.sh
+++ roboshop-shell/user.sh
@@ -1,21 +1,21 @@
 Log_file_location=/tmp/user.log
-echo -e"\e[32mCopying the user service and Mongo repo file to the desired location\e[0m" | tee -a $Log_file_location
+echo -e "\e[32mCopying the user service and Mongo repo file to the desired location\e[0m" | tee -a $Log_file_location
 cp user.service /etc/systemd/system/user.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e"\e[34mDownloading the node resource file\e[0m" | tee -a $Log_file_location
+echo -e "\e[34mDownloading the node resource file\e[0m" | tee -a $Log_file_location
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e"\e[34mInstalling the node JS \e[0m" | tee -a $Log_file_location
+echo -e "\e[34mInstalling the node JS \e[0m" | tee -a $Log_file_location
 yum install nodejs -y &>> $Log_file_location
 
-echo -e"\e[32mAdding roboshop user\e[0m" | tee -a $Log_file_location
+echo -e "\e[32mAdding roboshop user\e[0m" | tee -a $Log_file_location
 useradd roboshop
 
-echo -e"\e[33mCreating a directory\e[0m" | tee -a $Log_file_location
+echo -e "\e[33mCreating a directory\e[0m" | tee -a $Log_file_location
 mkdir /app
 
-echo -e"\e[33mDownloading the roboshop user code from DEV team\e[0m" | tee -a $Log_file_location
+echo -e "\e[33mDownloading the roboshop user code from DEV team\e[0m" | tee -a $Log_file_location
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 
 cd /app

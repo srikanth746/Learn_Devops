b766aa260ce6bea3ab38cd6d4645d50e8ee9588c
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-13 22:19:04 +0530

    Updated MYSQL shell script.

--- roboshop-shell/MySQL.sh
+++ roboshop-shell/MySQL.sh
@@ -1,19 +1,19 @@
 Log_file_location=/tmp/MySql.log
 #Removing the current default MySQL version
-echo -e"\e[31m Disabling the current default MYSQL version\e[0m" | tee -a $Log_file_location
+echo -e "\e[31m Disabling the current default MYSQL version\e[0m" | tee -a $Log_file_location
 yum module disable mysql -y &>> /dev/null
 
-echo -e"\e[32m copying the repo file\e[0m" | tee -a $Log_file_location
+echo -e "\e[32m copying the repo file\e[0m" | tee -a $Log_file_location
 cp MySQL.repo /etc/yum.repos.d/mysql.repo
 
-echo -e"\e[32m Installing the Mysql \e[0m" | tee -a $Log_file_location
+echo -e "\e[32m Installing the Mysql\e[0m" | tee -a $Log_file_location
 yum install mysql-community-server -y
 
 #Updating the password details to start service for the DB
-echo -e"\e[32m Updating the password details\e[0m" | tee -a $Log_file_location
+echo -e "\e[32m Updating the password details\e[0m" | tee -a $Log_file_location
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
 
-echo -e"\e[36m Enabling and restarting mysql\e[0m" | tee -a $Log_file_location
+echo -e "\e[36m Enabling and restarting mysql\e[0m" | tee -a $Log_file_location
 systemctl enable mysqld &>> /dev/null
 systemctl restart mysqld &>> $Log_file_location

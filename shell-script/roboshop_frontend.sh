f587caf6df685e06c5f0570d3aff1a1c0a9a68b7
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-14 17:01:50 +0530

    Updated functions method to the frontend shell script.

--- shell-script/roboshop_frontend.sh
+++ shell-script/roboshop_frontend.sh
@@ -1,9 +1,2 @@
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+source component.sh
+frontend

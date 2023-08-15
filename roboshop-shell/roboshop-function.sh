1d88126df1405f2f33f72e6f1ccb0e94770a2732
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-15 19:13:08 +0530

    Updated functions method for MySQL and robo-function shell script.

--- roboshop-shell/roboshop-function.sh
+++ roboshop-shell/roboshop-function.sh
@@ -180,3 +180,25 @@ func_cart(){
   echo -e "\e[31m Status of cart application\e[0m" | tee -a $Log_file_location
   systemctl status ${component} ; tail -f /var/log/messages &>> $Log_file_location
 }
+
+func_mysql(){
+  Log_file_location=/tmp/MySql.log
+  #Removing the current default MySQL version
+  echo -e "\e[31m Disabling the current default MYSQL version\e[0m" | tee -a $Log_file_location
+  yum module disable ${component} -y &>> /dev/null
+
+  echo -e "\e[32m copying the repo file\e[0m" | tee -a $Log_file_location
+  cp MySQL.repo /etc/yum.repos.d/${component}.repo
+
+  echo -e "\e[32m Installing the Mysql\e[0m" | tee -a $Log_file_location
+  yum install ${component}-community-server -y
+
+  #Updating the password details to start service for the DB
+  echo -e "\e[32m Updating the password details\e[0m" | tee -a $Log_file_location
+  mysql_secure_installation --set-root-pass RoboShop@1
+  mysql -uroot -pRoboShop@1
+
+  echo -e "\e[36m Enabling and restarting mysql\e[0m" | tee -a $Log_file_location
+  systemctl enable ${component}d &>> /dev/null
+  systemctl restart ${component}d &>> $Log_file_location
+}

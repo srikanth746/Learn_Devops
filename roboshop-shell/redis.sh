a6325e32b68bddea97ce457c0c21e584ac8939b5
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-15 16:54:29 +0530

    Updating functions for redis application of roboshop.

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -1,13 +1,3 @@
-Log_file_location=/tmp/redis.log
-
-echo -e "\e[34mInstalling the redis resource file from the official webpage\e[0m" | tee -a $Log_file_location
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-yum module enable redis:remi-6.2 -y &>> $Log_file_location
-yum install redis -y &>> $Log_file_location
-
-echo -e "\e[33mUpdating the listen address for the redis conf to 0.0.0.0\e[0m" | tee -a $Log_file_location
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
-
-echo -e "\e[32mEnabling and restarting the redis service\e[0m" | tee -a $Log_file_location
-systemctl enable redis &>> $Log_file_location
-systemctl restart redis &>> $Log_file_location
+component=redis
+source roboshop-function.sh
+func_redis
--- roboshop-shell/roboshop-function.sh
+++ roboshop-shell/roboshop-function.sh
@@ -81,3 +81,19 @@ func_catalogue(){
   systemctl enable catalogue &>> $Log_file_location
   systemctl restart catalogue &>> $Log_file_location
 }
+
+func_redis(){
+  Log_file_location=/tmp/${component}.log
+
+  echo -e "\e[34mInstalling the ${component} resource file from the official webpage\e[0m" | tee -a $Log_file_location
+  yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+  yum module enable redis:remi-6.2 -y &>> $Log_file_location
+  yum install ${component} -y &>> $Log_file_location
+
+  echo -e "\e[33mUpdating the listen address for the ${component} conf to 0.0.0.0\e[0m" | tee -a $Log_file_location
+  sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+
+  echo -e "\e[32mEnabling and restarting the ${component} service\e[0m" | tee -a $Log_file_location
+  systemctl enable ${component} &>> $Log_file_location
+  systemctl restart ${component} &>> $Log_file_location
+}

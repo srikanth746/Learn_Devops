e9a1a273708ee4ea2234ce949a82c32aac2aeb07
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-18 15:48:08 +0530

    Updated file with special vars, exit status check.

--- shellscript_practice/conditions.sh
+++ shellscript_practice/conditions.sh
@@ -1,8 +1,17 @@
 echo -e "\e[32m Please enter your current age\e[0m"
-read age
+age=$1
+echo -e "\e[33m Please enter your gender\e[0m"
+gender=$2
+if [ -z "${age}" ]
+then
+  echo "Missing age of the candidate"
+  exit
+fi
 if [ "$age" -gt 18 ]
 then
   echo "your are eligible for vote"
 else
   echo "you are not eligible to vote"
 fi
+echo total number of input $#
+echo list of inputs $*

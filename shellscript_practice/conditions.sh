66a34bed8929aae3c8317f833b4f2cc551f933ad
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-18 16:14:34 +0530

    Updated shell script of conditions.

--- shellscript_practice/conditions.sh
+++ shellscript_practice/conditions.sh
@@ -1,8 +1,3 @@
-if [ -z "${age}" ]
-then
-  echo "Missing age of the candidate"
-  exit
-fi
 echo -e "\e[32m Please enter your current age\e[0m"
 age=$1
 echo -e "\e[33m Please enter your gender\e[0m"

2e9aa663b707fe607f20c79ec804f0a81591c2e4
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-18 16:16:29 +0530

    Updated shell script of conditions.

--- shellscript_practice/conditions.sh
+++ shellscript_practice/conditions.sh
@@ -2,20 +2,11 @@ echo -e "\e[32m Please enter your current age\e[0m"
 age=$1
 echo -e "\e[33m Please enter your gender\e[0m"
 gender=$2
-if [ "$age" -gt 18 ]
+if [ "${age}" -gt 18 ]
 then
   echo "your are eligible for vote"
-  status=$?
 else
   echo "you are not eligible to vote"
-  status=$?
-fi
-
-if [ status -qe 0 ]
-then
-  echo "vote is successful"
-else
-  echo "Your vote is not valid"
 fi
 
 echo total number of input $#

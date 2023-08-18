a34cc7b84ffc2be1a9700ea3611851e9e0b72dac
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-18 16:13:01 +0530

    Updated the conditions shell script with exit status.

--- shellscript_practice/conditions.sh
+++ shellscript_practice/conditions.sh
@@ -1,12 +1,12 @@
-echo -e "\e[32m Please enter your current age\e[0m"
-age=$1
-echo -e "\e[33m Please enter your gender\e[0m"
-gender=$2
 if [ -z "${age}" ]
 then
   echo "Missing age of the candidate"
   exit
 fi
+echo -e "\e[32m Please enter your current age\e[0m"
+age=$1
+echo -e "\e[33m Please enter your gender\e[0m"
+gender=$2
 if [ "$age" -gt 18 ]
 then
   echo "your are eligible for vote"

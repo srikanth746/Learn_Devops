672bc543c63aaccac0b3c8ecfd55308198787f90
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-18 13:24:02 +0530

    Commit on echo command in shell-script.

---
+++ shellscript_practice/print.sh
@@ -0,0 +1,8 @@
+echo "Working to print the basic commands using shell script"
+
+echo -e "\e[31m>>>>Hello world<<<<<\e[0m"
+echo -e "\e[32m>>>>Welcome to devops<<<<\e[0m"
+echo -e "\e[33m>>>>hello world <<<<\e[0m"
+echo -e "\e[34m>>>>hello world <<<<\e[0m"
+echo -e "\e[35m>>>>hello world <<<<\e[0m"
+echo -e "\e[36m>>>>hello world <<<<\e[0m"

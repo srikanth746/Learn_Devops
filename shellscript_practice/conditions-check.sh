e1d9b588d779003764d4077d1b3537657e2e4333
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-19 18:31:28 +0530

    Updated the script with functions and vars

--- shellscript_practice/conditions-check.sh
+++ shellscript_practice/conditions-check.sh
@@ -2,3 +2,26 @@ found_nginx=sudo yum list installed | grep nginx
 if [ -z "${found_nginx}" ]; then
   echo "nginx is available"
 fi
+
+a=$1
+b=$2
+echo a
+echo b
+date_today=$(date)
+echo $date_today
+add=$((2+2))
+echo addition$add
+function input-args() {
+  echo "working on the function part to pass arguments from the main"
+  q=$1
+  w=$2
+  echo $*
+  echo $#
+  echo "printed all the type of special variables"
+  return 1
+  echo "executed the return command"
+
+}
+
+input-args 123 "abv"
+echo $?

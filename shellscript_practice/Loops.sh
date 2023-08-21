343e26aca63321498f3f3fdc36829bfe77347f1f
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-21 12:50:18 +0530

    loops version 1.0

--- shellscript_practice/Loops.sh
+++ shellscript_practice/Loops.sh
@@ -1,6 +1,7 @@
 echo -e "\e[34m Printing the patterns in shell script\e[0m"
 n=$1
-while [ "$n" -gt 0 ]; do
-  echo "hello"
-  n=$((n-1))
+i=0
+while [ "$n" -gt "$i" ]; do
+  echo "hello **"
+  i=$((i+1))
   done

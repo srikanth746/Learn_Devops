4a86246b491c4cb04535376ef6818efe0e23228e
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-21 12:54:49 +0530

    Updated loop script with the pattern printing.

--- shellscript_practice/Loops.sh
+++ shellscript_practice/Loops.sh
@@ -1,7 +1,17 @@
-echo -e "\e[34m Printing the patterns in shell script\e[0m"
+echo -e "\e[34m Working to understand basic loops concept in shell script \e[0m"
 n=$1
 i=0
 while [ "$n" -gt "$i" ]; do
   echo "hello **"
+  i=$((i+1))
+  done
+
+echo -e "\e[35m Printing the patterns in shell script\e[0m"
+while [ "$n" -gt "$i" ]; do
+  j=i
+  while [ "$j" -lt "$n" ]; do
+    echo "*"
+    j=$((j+1))
+    done
   i=$((i+1))
   done

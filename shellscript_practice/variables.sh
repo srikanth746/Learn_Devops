7ed9d9234416f9fe6d1883ad98553a75f481dd4b
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-21 12:20:07 +0530

    Updated the script with different variables.

---
+++ shellscript_practice/variables.sh
@@ -0,0 +1,9 @@
+echo -e "\e[32m Working on the variables printing in the shell script\e[0m"
+
+echo First variable argument -$1
+echo Second variable argument $2
+
+echo $*
+echo $#
+
+echo Printing environment variables - $abc

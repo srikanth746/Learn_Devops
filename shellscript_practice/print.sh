2fd9d852f7fc5a892b090f31d4025035f97c6a2c
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2023-08-21 11:09:22 +0530

    Added command to check the commit reset

--- shellscript_practice/print.sh
+++ shellscript_practice/print.sh
@@ -6,3 +6,5 @@ echo -e "\e[33m>>>>hello world <<<<\e[0m"
 echo -e "\e[34m>>>>hello world <<<<\e[0m"
 echo -e "\e[35m>>>>hello world <<<<\e[0m"
 echo -e "\e[36m>>>>hello world <<<<\e[0m"
+
+echo -e "\e[31m>>>>Checking the commit reset option<<<<\e[0m"

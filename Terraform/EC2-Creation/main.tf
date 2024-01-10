3838b9100b8626d78fef2b30c4b2561aef31f760
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2024-01-10 17:24:38 +0530

    created terraform script for ec2 instance.

---
+++ Terraform/EC2-Creation/main.tf
@@ -0,0 +1,24 @@
+#Requesting terraform for the specific provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+#Mentioned the required location
+provider "aws"{
+  region: "us-east-1"
+}
+
+#Creating the ec2 instance resource
+resource "aws_instance" "example" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-ubuntu"
+  }
+}

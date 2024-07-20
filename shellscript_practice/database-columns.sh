aab3e2421fe3d8a3cbdc4774e9ee4e0417b28ab1
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2024-07-20 19:11:14 +0530

    added if condition

--- shellscript_practice/database-columns.sh
+++ shellscript_practice/database-columns.sh
@@ -14,5 +14,13 @@ COLUMNS=$(mysql -u $USER -p$PASSWORD -h $HOST -D $DB_NAME -se "SHOW COLUMNS FROM
 echo "Columns in table $TABLE_NAME:"
 for i in $COLUMNS
 do
-    echo $i
+    col=$(echo "$i" | tr '[:upper:]' '[:lower:]')
+    if [ "$col" == "phone_number" ]; then
+        # code to execute if condition is true
+        echo "Please mask the data"
+    elif [ "$col" == "social_security_number" ]; then
+        # code to execute if another_condition is true
+        echo "Please mask SSN"
+    fi
+
 done

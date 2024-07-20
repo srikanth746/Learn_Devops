363441aecedcb0cddd164e8b0dac9444281faccd
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2024-07-20 19:38:45 +0530

    made changes to query

--- shellscript_practice/database-columns.sh
+++ shellscript_practice/database-columns.sh
@@ -9,7 +9,7 @@ TABLE_NAME="tmp_Users"
 
 # Fetch column names
 COLUMNS=$(mysql -u $USER -p$PASSWORD -h $HOST -D $DB_NAME -se "SHOW COLUMNS FROM $TABLE_NAME;" | awk '{print $1}')
-SQL_QUERY="UPDATE $TABLE_NAME SET phone_number = CONCAT(REPEAT('X', LENGTH(phone_number) - 4), SUBSTR(phone_number, -4));"
+#SQL_QUERY="UPDATE $TABLE_NAME SET phone_number = CONCAT(REPEAT('X', LENGTH(phone_number) - 4), SUBSTR(phone_number, -4));"
 
 # Print column names
 echo "Columns in table $TABLE_NAME:"
@@ -19,7 +19,7 @@ do
     if [[ "$col" == *"phone"* ]]; then
         # code to execute if condition is true
         echo "Masking the data for phone number"
-        mysql -u $USER -p$PASSWORD -h $HOST -D $DB_NAME -e "UPDATE $TABLE_NAME SET $col = CONCAT(REPEAT('X', LENGTH(phone_number) - 4), SUBSTR(phone_number, -4));"
+        mysql -u $USER -p$PASSWORD -h $HOST -D $DB_NAME -e "UPDATE $TABLE_NAME SET $col = CONCAT(REPEAT('X', LENGTH($col) - 4), SUBSTR($col, -4));"
 
     elif [[ "$col" == *"social"* ]]; then
         # code to execute if another_condition is true

c1cb78fb027198622e576d50cb13c05363ae4be0
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2024-07-20 19:44:42 +0530

    made changes to the shell script query

--- shellscript_practice/database-columns.sh
+++ shellscript_practice/database-columns.sh
@@ -19,12 +19,13 @@ do
     if [[ "$col" == *"phone"* ]]; then
         # code to execute if condition is true
         echo "Masking the data for phone number"
-        mysql -u $USER -p$PASSWORD -h $HOST -D $DB_NAME -e
+        mysql -h "$HOST" -u "$USER" -p"$PASSWORD" "$DB_NAME" -e \
         "UPDATE $TABLE_NAME SET $col = CONCAT(
-                                FLOOR(100 + (RAND() * 900)), '-',    -- Generate a 3-digit random number
-                                FLOOR(100 + (RAND() * 900)), '-',    -- Generate another 3-digit random number
-                                FLOOR(1000 + (RAND() * 9000))        -- Generate a 4-digit random number
-                            );"
+            FLOOR(100 + (RAND() * 900)), '-',    \
+            FLOOR(100 + (RAND() * 900)), '-',    \
+            FLOOR(1000 + (RAND() * 9000))        \
+        );"
+
 
     elif [[ "$col" == *"social"* ]]; then
         # code to execute if another_condition is true

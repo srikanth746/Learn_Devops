5fe2913e9f0a59822d521a1143709c1135d6e3c6
SRIKANTH <srikanth1999.viswanadhuni@gmail.com>
2024-07-21 19:11:08 +0530

    added the swt env

--- shellscript_practice/database-columns.sh
+++ shellscript_practice/database-columns.sh
@@ -110,8 +110,8 @@ sed -e 's/),(/\
 
 # Clean up
 rm table_dump.sql
-
-java -jar arx-3.9.1-gtk-64.jar -project config.xml -input "$OUTPUT_FILE" -output ./maskdata/masked_data_file.csv
+#java -Djava.library.path=/path/to/swt-libraries -jar arx.jar
+java -Djava.library.path=/usr/lib/jni/ -jar arx-3.9.1-gtk-64.jar -project config.xml -input "$OUTPUT_FILE" -output ./maskdata/masked_data_file.csv
 
 END=$(date +%s)
 
